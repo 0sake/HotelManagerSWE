decd0d4517f0cc00a9c58aa56cf3094c361ffba1
0sake <andrea.tatini22@gmail.com>
2022-11-27 17:42:48 +0100

    push002

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        User u0 = new User();
+        User u0 = new User("giovanni","pollo@gmail.com");
+        User u1 = new User("giacomo","rollo@gmail.com");
         u0.getName(u0);
+        u1.getEmail(u1);
     }
 }
--- User.java
+++ User.java
@@ -1,7 +1,17 @@
 public class User {
-    String name = "name";
+    protected String name = "name";
+    protected String email;
 
     public void getName(User u){
         System.out.println(u.name);
+    }
+
+    public void getEmail(User u){
+        System.out.println(u.email);
+    }
+
+    public User(String inputName ,String inputEmail ){
+        this.name = inputName;
+        this.email = inputEmail;
     }
 }
